2ce8b45936f5ce2f2c9376d20622a8c6de834d1d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-05-20 18:07:51 -0400

    preload uruntime instead

--- lapce-appimage.sh
+++ lapce-appimage.sh
@@ -9,6 +9,7 @@ UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.
 DESKTOP="https://github.com/lapce/lapce/raw/refs/heads/master/extra/linux/dev.lapce.lapce.desktop"
 ICON="https://raw.githubusercontent.com/lapce/lapce/eb83cee172efed14850dfe32e4bf7a5053fc2839/icons/lapce/lapce_logo.svg"
 URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-squashfs-lite-$ARCH"
+export URUNTIME_PRELOAD=1 # really needed here
 
 # lapce uses amd64 and arm64 instead
 if [ "$(uname -m)" = 'x86_64' ]; then
@@ -34,12 +35,6 @@ wget "$DESKTOP" -O        ./AppDir/lapce.desktop
 wget "$ICON"    -O        ./AppDir/dev.lapce.lapce.svg
 ln -s dev.lapce.lapce.svg ./AppDir/.DirIcon
 
-# We need to set the uruntime to never use FUSE because this application
-# has a built in terminal which FUSE will not let us elevate rights in it
-# also it actually doesn't work at all when FUSE is used 👀
-wget "$URUNTIME" -O ./uruntime
-sed -i 's|URUNTIME_EXTRACT=[0-9]|URUNTIME_EXTRACT=1|' ./uruntime
-
 wget "$APPIMAGETOOL" -O ./appimagetool
 chmod +x ./appimagetool
-./appimagetool -n -u "$UPINFO" ./AppDir --runtime-file ./uruntime
+./appimagetool -n -u "$UPINFO" ./AppDir
